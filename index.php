1b38e6eabf1d751e720daedc01e1027dcb31fd55
CedBG <you@example.com>
2023-03-20 16:39:06 -0400

    debut exercice

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Liste des écoles</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+  <body>
+
+<div class="container">
+
+    <header>
+        <div class="jumbotron">
+            <h1 class="display-4">Bienvenue sur la page d'accueil de votre école</h1>
+            <hr class="my-4">
+            <p class="lead">Accédez aux informations concernants les activités sportives de votre(vos) enfant(s) en fonction de son établissement</p>
+        </div>
+
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="ecole_a.php">école A</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="ecole_b.php">école B</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="ecole_c.php">école C</a>
+            </li>
+        </ul>
+    </header>
+        
+    <main>
+       
+        <img  src="./img/ecole.jpg"  class="object-fit-contain border rounded " whidth=300px height=auto alt="école">
+        
+    </main>
+
+    <footer>
+
+
+    </footer>
+</div>
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
--- table.sql
+++ table.sql
@@ -3,23 +3,30 @@ CREATE DATABASE php_expert_devoir1 ;
  
 USE php_expert_devoir1 ;
 
-CREATE TABLE ecole (
-    id_ecole INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    nom_ecole VARCHAR(20) NOT NULL ,
+CREATE TABLE ecoles (
+    id_ecoles INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    nom_ecoles VARCHAR(20) NOT NULL ,
 ) ENGINE = InnoDB;
 
-CREATE TABLE eleve (
-    id_eleve INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    nom_eleve VARCHAR(20) NOT NULL ,
-    prenom_eleve VARCHAR(20) NOT NULL ,
+INSERT INTO ecole (nom_ecole) VALUES('ecole_a','ecole_b','ecole_c')
+
+CREATE TABLE eleves (
+    id_eleves INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    nom_eleves VARCHAR(20) NOT NULL ,
+    prenom_eleves VARCHAR(20) NOT NULL ,
 ) ENGINE = InnoDB;
 
 CREATE TABLE sports (
     id_sports INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    boxe VARCHAR(20) NOT NULL ,
-    judo VARCHAR(20) NOT NULL ,
-    football VARCHAR(20) NOT NULL ,
-    natation VARCHAR(20) NOT NULL ,
-    cyclisme VARCHAR(20) NOT NULL ,
+    nom_sports VARCHAR(20) NOT NULL ,
 ) ENGINE = InnoDB;
+
+INSERT INTO sports (nom_sports) VALUES('boxe','judo','football','natation','cyclisme')
+
+CREATE TABLE sports_img (
+    id_sports__img INT(3) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    nom_sports__img VARCHAR(20) NOT NULL ,
+) ENGINE = InnoDB;
+
+INSERT INTO sports__img (nom_sports__img) VALUES('boxe.jpg','judo.jpg','foot.jpg','natation.jpg','cyclisme.jpg')
 
